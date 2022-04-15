f073607ba39da8dc92e382fee721b86fd39bc80f
neriosoft <alabaneri@gmail.com>
2022-04-15 01:21:45 +0100

    adding templates and template inheritance

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+# dummy post data
+posts = [
+    {
+        'author': 'Neriosoft',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'January 15, 2022'
+    },
+    {
+        'author': 'Teymebeauty',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': 'January 16, 2022'
+    },
+    {
+        'author': 'Oreofe',
+        'title': 'Blog Post 3',
+        'content': 'Third Post Content',
+        'date_posted': 'January 18, 2022'
+    }
+]
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request,'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request,'blog/about.html', {'title': 'About'})
